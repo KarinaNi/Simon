cf84327401b38811431b4450580d7cc77486f535
EKabaev <32227700+EKabaev@users.noreply.github.com>
2017-09-23 16:05:53 -0500

    Add files via upload

---
+++ Simon/src/SimonLogic.java
@@ -0,0 +1,8 @@
+
+public class SimonLogic {
+	private RandomSimonList randomList = new RandomSimonList();
+	
+	public boolean checkCorrect(int checked, int position) {
+		return randomList.getRandomList().get(position) == checked;
+	}
+}
