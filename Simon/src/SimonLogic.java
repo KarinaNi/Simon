4e31d476ed183ce77d5ab218da1af3387d15c5f2
KATERINAS\Katia.K <ekaterina.kabaev@gmail.com>
2017-09-23 16:42:08 -0500

    added 2 new methods

--- Simon/src/SimonLogic.java
+++ Simon/src/SimonLogic.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 /*
  * :)
  */
 public class SimonLogic {
+	
 	private RandomSimonList randomList = new RandomSimonList();
+	
+	public ArrayList<Integer> getList() {
+		return randomList.getRandomList();
+	}
+	
+	public void nextTurn() {
+		randomList.getNextNumber();
+	}
 	
 	public boolean checkCorrect(int checked, int position) {
 		return randomList.getRandomList().get(position) == checked;
