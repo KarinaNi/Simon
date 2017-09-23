3f8836db8bfd6aca52e5c95cd2cc8a0fbc13f586
EKabaev <32227700+EKabaev@users.noreply.github.com>
2017-09-23 17:14:06 -0500

    Add files via upload

---
+++ Simon/src/SimonLogic.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/*
+ * :(
+ */
+public class SimonLogic {
+	
+	private RandomSimonList randomList = new RandomSimonList();
+	
+	public ArrayList<Integer> getList() {
+		return randomList.getRandomList();
+	}
+	
+	public void nextTurn() {
+		randomList.getNextNumber();
+	}
+	
+	public boolean checkCorrect(int checked, int position) {
+		return randomList.getRandomList().get(position) == checked;
+	}
+}
