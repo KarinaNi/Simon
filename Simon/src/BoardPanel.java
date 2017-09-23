e452c76d7fcdc98ffe906e025919762e7b497633
Mike White <mswhite3@wisc.edu>
2017-09-23 15:55:32 -0500

    updated boardpanel

--- Simon/src/BoardPanel.java
+++ Simon/src/BoardPanel.java
@@ -1,10 +1,35 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
-import javax.swing.JLayeredPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
-public class BoardPanel extends JLayeredPane
+public class BoardPanel extends JPanel
 {
+	JFrame frame;
+	JLabel title;
+	
 	public BoardPanel()
 	{
-		
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setLayout(new FlowLayout());
+	    
+	    title = new JLabel("Welcome to Simon, bitch");
+	    title.setFont(new Font("Courier", Font.PLAIN, 32)); 
+	    title.setHorizontalAlignment(SwingConstants.CENTER);
+	    title.setVerticalAlignment(SwingConstants.TOP);
+	    title.setOpaque(true);
+	    title.setBackground(Color.black);
+	    title.setForeground(Color.white);
+	    title.setPreferredSize(new Dimension(600, 400)); 
+	    
+	    frame.add(title);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
--- Simon/src/Main.java
+++ Simon/src/Main.java
@@ -2,7 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BoardPanel boardpanel = new BoardPanel();
+		
+		
+		
 
 	}
 
