35adfa313122ffb9c2586141114fd06b25e223be
Daniel Miller <17dmiller2400@gmail.com>
2017-09-23 17:02:22 -0500

    In progress audio

---
+++ Simon/src/Audio.java
@@ -0,0 +1,8 @@
+
+public class Audio {
+    private final String audioLoc1 = "";
+    private final String audioLoc2 = "";
+    private final String audioLoc3 = "";
+    private final String audioLoc4 = "";
+
+}
